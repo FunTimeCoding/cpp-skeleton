2ece7c6ca9881c68249c4e0720a9bd23f6d79e4d
Alexander Reitzel <funtimecoding@gmail.com>
2020-06-30 22:57:16 +0200

    synchronize with skeleton

--- script/build.sh
+++ script/build.sh
@@ -2,8 +2,18 @@
 
 rm -rf build
 script/check.sh --ci-mode
+script/measure.sh --ci-mode
+script/test.sh --ci-mode
 g++ -std=c++11 src/boilerplate.cpp -o build/bp
 
 if [ "${1}" = --run ]; then
     build/bp
 fi
+
+#SYSTEM=$(uname)
+#
+# TODO: Needs polish.
+#if [ "${SYSTEM}" = Linux ]; then
+#    script/debian/package.sh
+#    script/docker/build.sh
+#fi
